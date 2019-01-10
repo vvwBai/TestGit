0c23a336534fa925c755b592e15d905ed64bbb74
vvwBai <447055767@qq.com>
2019-01-10 18:00:54 +0800

    添加了Zuul的微服务网关

---
+++ microservice-file-upload/src/main/java/com/bai/cloud/study/MicroserviceFileUploadApplication.java
@@ -0,0 +1,16 @@
+package com.bai.cloud.study;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableEurekaClient
+public class MicroserviceFileUploadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroserviceFileUploadApplication.class, args);
+	}
+
+}
+

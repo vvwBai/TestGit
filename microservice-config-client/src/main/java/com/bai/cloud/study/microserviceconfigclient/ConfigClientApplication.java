cff9fdc18195f28f8127797570ad5a1d23a1acf9
vvwBai <447055767@qq.com>
2019-01-17 17:49:42 +0800

    增加 ZipKin

---
+++ microservice-config-client/src/main/java/com/bai/cloud/study/microserviceconfigclient/ConfigClientApplication.java
@@ -0,0 +1,14 @@
+package com.bai.cloud.study.microserviceconfigclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfigClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigClientApplication.class, args);
+	}
+
+}
+
---
+++ microservice-trace-zipkin-server/src/test/java/com/bai/cloud/study/microservicetracezipkinserver/MicroserviceTraceZipkinServerApplicationTests.java
@@ -0,0 +1,17 @@
+package com.bai.cloud.study.microservicetracezipkinserver;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MicroserviceTraceZipkinServerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
